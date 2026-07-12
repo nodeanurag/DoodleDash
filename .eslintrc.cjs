5ce8d439411ad5fc2b5b44eac71007d8b0aa4e57
nodeanurag <conveytoanurag@gmail.com>
2026-07-12 14:58:21 +0530

    feat: add ESLint configuration and package.json for client setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  env: {
+    node: true,
+    browser: true,
+    es2022: true,
+  },
+  ignorePatterns: ['dist', 'build', 'node_modules', '*.cjs', 'vite.config.ts'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
